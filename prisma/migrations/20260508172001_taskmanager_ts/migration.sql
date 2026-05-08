c3106e8ff6285afbbf87da2450c8dd6f369d6ca0
chknutson <knutsonc87+git@gmail.com>
2026-05-08 11:21:40 -0600

    initial Commit

---
+++ prisma/migrations/20260508172001_taskmanager_ts/migration.sql
@@ -0,0 +1,32 @@
+-- CreateEnum
+CREATE TYPE "Category" AS ENUM ('Work', 'Home', 'Fittness', 'Family', 'Personal');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "task" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "due_date" TIMESTAMP(3),
+    "category" "Category" NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
