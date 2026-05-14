5ede88c03ff444ffc949092924061339650d9f1d
chknutson <knutsonc87+git@gmail.com>
2026-05-14 14:10:28 -0600

    dynamic routing started

---
+++ src/app/api/tasks/[id]/route.ts
@@ -0,0 +1,51 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Category, PrismaClient } from "@/generated/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient({
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
+  });
+
+  export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+  
+    const task = await prisma.task.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+  
+    return NextResponse.json(task);
+}
+  
+export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    const body = await request.json();
+  
+    const task = await prisma.task.update({
+        where: { id: Number(id) },
+        //     data: {
+        //         task: body.task,
+        //         description: body.description,
+        //         category: body.category as Category,
+        //         due_date: body.due_date ? new Date(body.due_date) : null,
+        //     },
+        // })
+        data: {
+            ...(body.task !== undefined && { task: body.task }),
+            ...(body.description !== undefined && { description: body.description }),
+            ...(body.category !== undefined && { category: body.category as Category }),
+            ...(body.due_date !== undefined && { due_date: body.due_date ? new Date(body.due_date) : null, }),
+        },
+    });
+    return NextResponse.json(task);
+
+}
+
+
+
+// export async function DELETE(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+
+
+//     return NextResponse.json(task);
+// }
--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/generated/prisma/client";
+import { Category, PrismaClient } from "@/generated/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient({
@@ -8,9 +8,27 @@ const prisma = new PrismaClient({
 
 // const users = await prisma.user.findMany();  Template to use use in function
 
-function GET(request: NextRequest): NextResponse<string> {
+export async function GET(request: NextRequest) { //}: Promise<NextResponse<string>> {
     
-    const tasks = prisma.task.findMany()
+    const tasks = await prisma.task.findMany({
+    })
 
-    return NextResponse.json (body: "return value")
+    return NextResponse.json (tasks)
 }
+
+export async function POST(request: NextRequest) { //: NextResponse<string> {
+    const body = await request.json();
+
+    const task = await prisma.task.create({
+        data: {
+            task: body.task,
+            description: body.description,
+            category: body.category as Category,
+            due_date: body.due_date ? new Date(body.due_date) : null,
+            user_id: 1, // or omit entirely
+        },
+    })
+
+    return NextResponse.json(task)
+}
+
