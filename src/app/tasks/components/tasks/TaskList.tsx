40dd724dd67fbfe75ed9872417ee363192ed66c2
chknutson <knutsonc87+git@gmail.com>
2026-06-09 10:34:31 -0600

    added edit and delete buttons

--- src/app/tasks/components/tasks/TaskList.tsx
+++ src/app/tasks/components/tasks/TaskList.tsx
@@ -1,3 +1,8 @@
+import { deleteTask } from "@/app/actions/deleteTask"
+import { updateTask } from "@/app/actions/updateTask"
+import Link from "next/link"
+
+
 type Task = {
     id: number
     task: string
@@ -12,8 +17,33 @@ type Task = {
     return (
       <div>
         {tasks.map((t) => (
-          <div key={t.id}>
-            {t.task}
+          // <div key={t.id}>
+          //   {t.task}
+          // </div>
+          <div key={t.id} className="flex gap-4">
+            <span>{t.task}</span>
+
+            <Link
+              href={`/tasks/${t.id}/edit`}
+              className="text-blue-600"
+            >
+              Edit
+            </Link>
+
+            <form action={deleteTask}>
+              <input
+                type="hidden"
+                name="id"
+                value={t.id}
+              />
+
+              <button
+                type="submit"
+                className="text-red-600"
+              >
+                Delete
+              </button>
+            </form>
           </div>
         ))}
       </div>
